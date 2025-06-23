1fd1d62adfac9209f667cbd098ca80fcb0981a65
Eiud Waititu <eliudgachuki130@gmail.com>
2025-06-23 14:06:30 +0300

    Did some upgrades

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const handleCall = async () => {
+    const phoneNumber = "+254719790026";
+    try {
+      await navigator.clipboard.writeText(phoneNumber);
+      
+      window.location.href = `tel:${phoneNumber}`;
+    } catch (err) {
+      console.error("Failed to copy number: ", err);
+      alert("Failed to copy phone number.");
+    }
+  };
